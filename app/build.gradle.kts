0a8998e23cd28eabe9dbd82f4c5315b2d59e17b6
David Yoon <qqpo12@naver.com>
2024-12-05 20:41:59 +0900

    Add a mainView
    * Added a recommendMenuList to display item_menu

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,6 +50,8 @@ dependencies {
 
     implementation("androidx.navigation:navigation-fragment-ktx:2.8.4")
     implementation("androidx.navigation:navigation-ui-ktx:2.8.4")
+    implementation("com.google.code.gson:gson:2.11.0")
+    implementation("com.github.bumptech.glide:glide:4.16.0")
 
 
     testImplementation(libs.junit)
--- app/src/main/java/com/example/fcstarbucks/HomeFragment.kt
+++ app/src/main/java/com/example/fcstarbucks/HomeFragment.kt
@@ -1,8 +1,10 @@
 package com.example.fcstarbucks
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import androidx.fragment.app.Fragment
+import com.bumptech.glide.Glide
 import com.example.fcstarbucks.databinding.FragmentHomeBinding
 
 class HomeFragment: Fragment(R.layout.fragment_home) {
@@ -13,6 +15,27 @@ class HomeFragment: Fragment(R.layout.fragment_home) {
         super.onViewCreated(view, savedInstanceState)
 
         binding = FragmentHomeBinding.bind(view)
+
+        val homeData = context?.readData() ?: return
+        Log.e("HomeFragment", homeData.appbarImage)
+        binding.appBarTitleTextView.text = getString(R.string.appbar_title_text, homeData.user.nickname)
+        binding.starCountTextView.text = getString(R.string.appbar_star_title, homeData.user.starCount, homeData.user.totalCount)
+
+        binding.appBarProgressBar.max = homeData.user.totalCount
+        binding.appBarProgressBar.progress = homeData.user.starCount
+
+        Glide.with(binding.appBarImageView)
+            .load(homeData.appbarImage)
+            .into(binding.appBarImageView)
+
+
+
+        binding.recommendMenuList.menuLayout.addView(
+            MenuView(requireContext()).apply {
+                setTitle("아이스 카페 라떼")
+                setImageUrl("https://picsum.photos/200/100")
+            }
+        )
     }
 
 }
